40757030db332c13e528b5054e955f86693a5d7f
tianyuan <tiany1992@sina.com>
2016-10-21 23:53:18 +0800

    装饰模式
    Signed-off-by: tianyuan <tiany1992@sina.com>

--- Java DesignPattern/004DecoratorFactory.java
+++ Java DesignPattern/004DecoratorFactory.java
@@ -1,10 +1,10 @@
 public abstract class Component{
-    public abstract void Operation();
+    public abstract void operation();
 }
 
 public class ConcreteComponent extends Component{
     @Override
-    public void Operation(){
+    public void operation(){
         //具体对象的操作
     }
 }
@@ -12,22 +12,22 @@ public class ConcreteComponent extends Component{
 public abstract class Decorator extends Component{
     protected Component component;
 
-    public void SetComponent(Component component){
+    public void setComponent(Component component){
         this.component=component;
     }
 
     @Override
-    public void Operation(){
+    public void operation(){
         if(component!=null){
-            component.Operation();
+            component.operation();
         }
     }
 }
 
 public class ConcreteDecoratorA extends Decorator{
     @Override
-    public void Operation(){
-        base.Operation();
+    public void operation(){
+        super.Operation();
         //具体装饰对象A的操作
     }
 
@@ -36,8 +36,8 @@ public class ConcreteDecoratorA extends Decorator{
 
 public class ConcreteDecoratorB extends Decorator{
     @Override
-    public void Operation(){
-        base.Operation();
+    public void operation(){
+        super.Operation();
         //具体装饰对象B的操作
     }
 
@@ -49,9 +49,9 @@ public static Main(String[] args){
     ConcreteDecoratorA d1=new ConcreteDecoratorA();
     ConcreteDecoratorB d2=new ConcreteDecoratorB();
 
-    d1.SetComponent(c);
-    d2.SetComponent(d1);
-    d2.Operation();
+    d1.setComponent(c);
+    d2.setComponent(d1);
+    d2.operation();
     /*
     装饰的方法是：首先用ConcreteComponent实例化对象c，然后用ConcreteDecoratorA的
     实例化对象d1来包装c,再用ConcreteDecoratorB的对象d2包装d1，最终执行d2的Operation()
