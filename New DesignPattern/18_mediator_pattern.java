b295e493520faa18e45b62a3b7dc3ce4d11ab4ab
xiaolang124 <tiany1992@sina.com>
2017-10-17 17:20:38 +0800

    mediator pattern

---
+++ New DesignPattern/18_mediator_pattern.java
@@ -0,0 +1,44 @@
+//创建中介类。
+//	ChatRoom.java
+import java.util.Date;
+
+public class ChatRoom {
+   public static void showMessage(User user, String message){
+      System.out.println(new Date().toString()
+         + " [" + user.getName() +"] : " + message);
+   }
+}
+
+//创建 user 类。
+//	User.java
+public class User {
+   private String name;
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public User(String name){
+      this.name  = name;
+   }
+
+   public void sendMessage(String message){
+      ChatRoom.showMessage(this,message);
+   }
+}
+
+//使用 User 对象来显示他们之间的通信。
+//	MediatorPatternDemo.java
+public class MediatorPatternDemo {
+   public static void main(String[] args) {
+      User robert = new User("Robert");
+      User john = new User("John");
+
+      robert.sendMessage("Hi! John!");
+      john.sendMessage("Hello! Robert!");
+   }
+}
